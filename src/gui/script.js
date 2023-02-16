93c859ec47fe1d42b79d336d98ed034b27a81819
Jacky Song <almey0721@gmail.com>
2023-02-16 11:58:21 -0500

    Beginning of refactor

--- src/gui/script.js
+++ src/gui/script.js
@@ -1,324 +1,3 @@
-// Switch between opened files (multi-file editing)
-
-// run the functions onclick
-document.getElementById("f1").addEventListener("click", editPage_1);
-document.getElementById("f2").addEventListener("click", editPage_2);
-document.getElementById("f3").addEventListener("click", editPage_3);
-document.getElementById("f4").addEventListener("click", editPage_4);
-document.getElementById("f5").addEventListener("click", editPage_5);
-
-document.getElementById("bootstrap-zoom-icon").addEventListener("click", zoom_in);
-document.getElementById("bootstrap-shrink-icon").addEventListener("click", zoom_out);
-
-
-// Make code focused
-
-document.getElementById("page-1").focus();
-
-
-// Displays the selected file based on its ID and hides all others
-
-function editPage_1() {
-    // Make page 1 appear
-    document.getElementById("page-1").style.display = "block";
-    // Make all other pages disappear
-    document.getElementById("page-2").style.display = "none";
-    document.getElementById("page-3").style.display = "none";
-    document.getElementById("page-4").style.display = "none";
-    document.getElementById("page-5").style.display = "none";
-    // Highlight the page 1 buttton
-    document.getElementById("f1").style.backgroundColor = "#001d35";
-        // Remove highlight on all other buttons
-        document.getElementById("f2").style.background = "none";
-    document.getElementById("f3").style.background = "none";
-    document.getElementById("f4").style.background = "none";
-    document.getElementById("f5").style.background = "none";
-    // Make highlight on hover work again
-    highlightOnHover_1();
-}
-
-function highlightOnHover_1() {
-    document.getElementById("f2").addEventListener("mouseover", function() {
-        document.getElementById("f2").style.backgroundColor = "#193349";
-    });
-    document.getElementById("f2").addEventListener("mouseout", function() {
-        document.getElementById("f2").style.backgroundColor = "#00101d";
-    });
-
-    document.getElementById("f2").addEventListener("mouseover", function() {
-        document.getElementById("f2").style.backgroundColor = "#193349";
-    });
-    document.getElementById("f2").addEventListener("mouseout", function() {
-        document.getElementById("f2").style.backgroundColor = "#00101d";
-    });
-
-    document.getElementById("f3").addEventListener("mouseover", function() {
-        document.getElementById("f3").style.backgroundColor = "#193349";
-    });
-    document.getElementById("f3").addEventListener("mouseout", function() {
-        document.getElementById("f3").style.backgroundColor = "#00101d";
-    });
-
-    document.getElementById("f5").addEventListener("mouseover", function() {
-        document.getElementById("f5").style.backgroundColor = "#193349";
-    });
-    document.getElementById("f5").addEventListener("mouseout", function() {
-        document.getElementById("f5").style.backgroundColor = "#00101d";
-    });
-}
-
-function editPage_2() {
-    // Make page 2 appear
-    document.getElementById("page-2").style.display = "block";
-    // Make all other pages disappear
-    document.getElementById("page-1").style.display = "none";
-    document.getElementById("page-3").style.display = "none";
-    document.getElementById("page-4").style.display = "none";
-    document.getElementById("page-5").style.display = "none";
-    // Highlight the page 2 buttton
-    document.getElementById("f2").style.backgroundColor = "#001d35";
-        // Remove highlight on all other buttons
-        document.getElementById("f1").style.background = "none";
-    document.getElementById("f3").style.background = "none";
-    document.getElementById("f4").style.background = "none";
-    document.getElementById("f5").style.background = "none";
-    // Make highlight on hover work again
-    highlightOnHover_2();
-}
-
-function highlightOnHover_2() {
-    document.getElementById("f1").addEventListener("mouseover", function() {
-        document.getElementById("f1").style.backgroundColor = "#193349";
-    });
-    document.getElementById("f1").addEventListener("mouseout", function() {
-        document.getElementById("f1").style.backgroundColor = "#00101d";
-    });
-
-    document.getElementById("f4").addEventListener("mouseover", function() {
-        document.getElementById("f4").style.backgroundColor = "#193349";
-    });
-    document.getElementById("f4").addEventListener("mouseout", function() {
-        document.getElementById("f4").style.backgroundColor = "#00101d";
-    });
-
-    document.getElementById("f3").addEventListener("mouseover", function() {
-        document.getElementById("f3").style.backgroundColor = "#193349";
-    });
-    document.getElementById("f3").addEventListener("mouseout", function() {
-        document.getElementById("f3").style.backgroundColor = "#00101d";
-    });
-
-    document.getElementById("f5").addEventListener("mouseover", function() {
-        document.getElementById("f5").style.backgroundColor = "#193349";
-    });
-    document.getElementById("f5").addEventListener("mouseout", function() {
-        document.getElementById("f5").style.backgroundColor = "#00101d";
-    });
-}
-
-function editPage_3() {
-    // Make page 3 appear
-    document.getElementById("page-3").style.display = "block";
-    // Make all other pages disappear
-    document.getElementById("page-1").style.display = "none";
-    document.getElementById("page-2").style.display = "none";
-    document.getElementById("page-4").style.display = "none";
-    document.getElementById("page-5").style.display = "none";
-    // Highlight the page 3 buttton
-    document.getElementById("f3").style.backgroundColor = "#001d35";
-        // Remove highlight on all other buttons
-        document.getElementById("f1").style.background = "none";
-    document.getElementById("f2").style.background = "none";
-    document.getElementById("f4").style.background = "none";
-    document.getElementById("f5").style.background = "none";
-    // Make highlight on hover work again
-    highlightOnHover_3();
-}
-
-function highlightOnHover_3() {
-    document.getElementById("f1").addEventListener("mouseover", function() {
-        document.getElementById("f1").style.backgroundColor = "#193349";
-    });
-    document.getElementById("f1").addEventListener("mouseout", function() {
-        document.getElementById("f1").style.backgroundColor = "#00101d";
-    });
-
-    document.getElementById("f2").addEventListener("mouseover", function() {
-        document.getElementById("f2").style.backgroundColor = "#193349";
-    });
-    document.getElementById("f2").addEventListener("mouseout", function() {
-        document.getElementById("f2").style.backgroundColor = "#00101d";
-    });
-
-    document.getElementById("f4").addEventListener("mouseover", function() {
-        document.getElementById("f4").style.backgroundColor = "#193349";
-    });
-    document.getElementById("f4").addEventListener("mouseout", function() {
-        document.getElementById("f4").style.backgroundColor = "#00101d";
-    });
-
-    document.getElementById("f5").addEventListener("mouseover", function() {
-        document.getElementById("f5").style.backgroundColor = "#193349";
-    });
-    document.getElementById("f5").addEventListener("mouseout", function() {
-        document.getElementById("f5").style.backgroundColor = "#00101d";
-    });
-}
-
-function editPage_4() {
-    // Make page 4 appear
-    document.getElementById("page-4").style.display = "block";
-    // Make all other pages disappear
-    document.getElementById("page-1").style.display = "none";
-    document.getElementById("page-3").style.display = "none";
-    document.getElementById("page-2").style.display = "none";
-    document.getElementById("page-5").style.display = "none";
-    // Highlight the page 4 buttton
-    document.getElementById("f4").style.backgroundColor = "#001d35";
-        // Remove highlight on all other buttons
-        document.getElementById("f2").style.background = "none";
-    document.getElementById("f3").style.background = "none";
-    document.getElementById("f1").style.background = "none";
-    document.getElementById("f5").style.background = "none";
-    // Make highlight on hover work again
-    highlightOnHover_4();
-}
-
-function highlightOnHover_4() {
-    document.getElementById("f1").addEventListener("mouseover", function() {
-        document.getElementById("f1").style.backgroundColor = "#193349";
-    });
-    document.getElementById("f1").addEventListener("mouseout", function() {
-        document.getElementById("f1").style.backgroundColor = "#00101d";
-    });
-
-    document.getElementById("f2").addEventListener("mouseover", function() {
-        document.getElementById("f2").style.backgroundColor = "#193349";
-    });
-    document.getElementById("f2").addEventListener("mouseout", function() {
-        document.getElementById("f2").style.backgroundColor = "#00101d";
-    });
-
-    document.getElementById("f3").addEventListener("mouseover", function() {
-        document.getElementById("f3").style.backgroundColor = "#193349";
-    });
-    document.getElementById("f3").addEventListener("mouseout", function() {
-        document.getElementById("f3").style.backgroundColor = "#00101d";
-    });
-
-    document.getElementById("f5").addEventListener("mouseover", function() {
-        document.getElementById("f5").style.backgroundColor = "#193349";
-    });
-    document.getElementById("f5").addEventListener("mouseout", function() {
-        document.getElementById("f5").style.backgroundColor = "#00101d";
-    });
-}
-
-function editPage_5() {
-    // Make page 5 appear
-    document.getElementById("page-5").style.display = "block";
-    // Make all other pages disappear
-    document.getElementById("page-1").style.display = "none";
-    document.getElementById("page-3").style.display = "none";
-    document.getElementById("page-4").style.display = "none";
-    document.getElementById("page-2").style.display = "none";
-    // Highlight the page 5 buttton
-    document.getElementById("f5").style.backgroundColor = "#001d35";
-        // Remove highlight on all other buttons
-        document.getElementById("f2").style.background = "none";
-    document.getElementById("f3").style.background = "none";
-    document.getElementById("f4").style.background = "none";
-    document.getElementById("f1").style.background = "none";
-    // Make highlight on hover work again
-    highlightOnHover_5();
-}
-
-function highlightOnHover_5() {
-    document.getElementById("f1").addEventListener("mouseover", function() {
-        document.getElementById("f1").style.backgroundColor = "#193349";
-    });
-    document.getElementById("f1").addEventListener("mouseout", function() {
-        document.getElementById("f1").style.backgroundColor = "#00101d";
-    });
-
-    document.getElementById("f2").addEventListener("mouseover", function() {
-        document.getElementById("f2").style.backgroundColor = "#193349";
-    });
-    document.getElementById("f2").addEventListener("mouseout", function() {
-        document.getElementById("f2").style.backgroundColor = "#00101d";
-    });
-
-    document.getElementById("f3").addEventListener("mouseover", function() {
-        document.getElementById("f3").style.backgroundColor = "#193349";
-    });
-    document.getElementById("f3").addEventListener("mouseout", function() {
-        document.getElementById("f3").style.backgroundColor = "#00101d";
-    });
-
-    document.getElementById("f4").addEventListener("mouseover", function() {
-        document.getElementById("f4").style.backgroundColor = "#193349";
-    });
-    document.getElementById("f4").addEventListener("mouseout", function() {
-        document.getElementById("f4").style.backgroundColor = "#00101d";
-    });
-
-}
-
-
-// The about page
-
-function about_on() {
-    document.getElementById("overlay").style.display = "block";
-}
-
-function about_off() {
-    document.getElementById("overlay").style.display = "none";
-}
-
-// Basic clipboard operations
-
-// The copy() function
-function copy() {
-    document.getElementById("txt-field").focus(); //make selected active
-    document.execCommand("copy"); //copy selected text
-    log("Copied text to clipboard");
-}
-
-// The cut() function
-function cut() {
-    document.getElementById("txt-field").focus(); //make selected active
-    document.execCommand("cut"); //cut selected text
-    log("Cut text to clipboard");
-}
-
-// The paste() function
-async function paste() {
-    document.getElementById("txt-field").focus(); //make selected active
-    if (!navigator.clipboard) {
-        // Clipboard API not available
-        alert("You'll don't have clipboard permissions enabled");
-    }
-    try {
-        const text = await navigator.clipboard.readText();
-        document.getElementById("page-1").innerText = document.getElementById("page-1").innerText + text;
-        log("Text pasted.");
-    } catch {
-        log("Failed to read clipboard");
-    }
-}
-
-// Strip any pasted text of formatting (courtesy of pimvdb from Stack Overflow)
-document.getElementById("txt-field").addEventListener("paste", function(e) {
-    // cancel paste
-    e.preventDefault();
-
-    // get text representation of clipboard
-    var text = (e.originalEvent || e).clipboardData.getData("text/plain");
-
-    // insert text manually
-    document.execCommand("insertHTML", false, text);
-});
-
 // For toast notifications
 function log(value) {
     clearTimeout(log.timer);
@@ -334,88 +13,117 @@ function log(value) {
 // Undo/Redo operations
 
 function undo() {
-    document.querySelector("code").focus(); //make selected active
+    document.querySelector("textarea").focus(); //make selected active
     document.execCommand("undo");
 }
 
 function redo() {
-    document.querySelector("code").focus(); //make selected active
+    document.querySelector("textarea").focus(); //make selected active
     document.execCommand("redo");
 }
 
-// Opening file from disk
+const menubar = document.querySelector("#menubar");
+const textarea = document.querySelector("textarea");
+const lineNumbers = document.querySelector('.line-numbers')
 
-function readFile() {
-	document.getElementById("inputfile").click();
-    readFile2();
+let menus = {
+  "File": {},
+  "Edit": {},
+  "View": {},
+  "Settings": {},
+  "Help": {}
 }
 
-function readFile2() {
-    
-	document.getElementById("inputfile") 
-        .addEventListener("change", function() { 
-          
-        var fr=new FileReader(); 
-        fr.onload=function(){ 
-            document.getElementById("page-1").textContent=fr.result; 
-        } 
-          
-        fr.readAsText(this.files[0]); 
-    }) 
+function addMenuElement(menu, name, description) {
+  menus[menu][name] = description
 }
 
+addMenuElement("File", "new", "New");
+addMenuElement("File", "save", "Save");
+addMenuElement("File", "open", "Open file");
+addMenuElement("File", "openRecent", "Open recent");
+addMenuElement("File", "openLastSession", "Reopen last session");
+addMenuElement("Edit", "undo", "Undo");
+addMenuElement("Edit", "redo", "Redo");
+addMenuElement("Edit", "cut", "Cut");
+addMenuElement("Edit", "copy", "Copy");
+addMenuElement("Edit", "paste", "Paste");
 
-// Saving files to disk
+let menus_list = Object.keys(menus);
 
-function save(){
-  var text = document.getElementById("txt-field").innerText;
-  document.getElementById('saveButton').setAttribute(
-    'href',
-    'data:Content-type: text/plain, ' + escape(text)
-  );
-  log("A copy was saved!");
+function createMenubarElement(name) {
+  let menubarElement = document.createElement("div");
+  menubarElement.classList.add("dropdown");
+  menubarElement.innerHTML = `
+    <div class="dropdown-button hvr-radial-out">
+      <p>${name}</p>
+    </div>
+  `;
+  let menuContents = createMenuContents(name);
+  menubarElement.appendChild(menuContents);
+  return menubarElement
 }
 
-// Saving files to localStorage
-function saveEdits() {
-    //get the editable element
-    var editElem = document.getElementById("page-1");
-
-    //get the edited element content
-    var userVersion = editElem.innerHTML;
-
-    //save the content to local storage
-    localStorage.userEdits = userVersion;
-
-    //write a confirmation to the user
-    log("Edits saved!");
+function createMenuContents(name) {
+  let menuContents = document.createElement("div");
+  menuContents.classList.add("dropdown-content");
+  let submenus = Object.keys(menus[name]);
+  console.log(submenus)
+  for (let submenu of submenus) {
+    let description = menus[name][submenu]
+    let label = document.createElement("p");
+    label.innerText = description;
+    label.dataset.menuId = submenu;
+    menuContents.appendChild(label);
+    console.log(label)
+  }
+  return menuContents
 }
 
-// Right sidebar buttons
-function zoom_in() {
-    code = document.getElementById("txt-field");
-    code.style.fontSize = "120%" ;
+function newFile() {
+  log("Created new file");
 }
 
-function zoom_out() {
-    code = document.getElementById("txt-field");
-    code.style.fontSize = "80%" ;
+function saveFile() {
+  log("Quark auto-saves your work :)");
 }
 
-// Keyboard shortcut bindings
+for (let menuItem of menus_list) {
+  let menuElement = createMenubarElement(menuItem);
+  let menuContents = document.createElement("div");
+  menubar.appendChild(menuElement);
+}
 
-// Alt S (Save)
-keyboardJS.bind("alt + s", (e) => {
-  saveEdits();
+// Event delegation for menubar elements
+menubar.addEventListener("click", function(event) {
+  let target = event.target;
+  console.log(target.dataset.menuId);
+  switch (target.dataset.menuId) {
+    case "new":
+      newFile();
+      break;
+  }
 });
 
-// Alt S C (Save copy)
+window.addEventListener('DOMContentLoaded', handleLineNumbers);
+textarea.addEventListener('keyup', handleLineNumbers);
 
-keyboardJS.bind("alt + c", (e) => {
-  document.getElementById("saveButton").click();
-});
+function handleLineNumbers() {
+  const numberOfLines = textarea.value.split('\n').length
 
-// Alt N (New file)
-keyboardJS.bind("alt + n", (e) => {
-  editPage_5();
-});
+  lineNumbers.innerHTML = Array(numberOfLines)
+    .fill('<span></span>')
+    .join('')
+}
+
+function textareaSetContent() {
+  handleLineNumbers();
+  let textContent = textarea.value;
+  localStorage.setItem("textContent", textContent);
+}
+
+function textareaLoadContent() {
+  handleLineNumbers();
+  let textContent = localStorage.getItem("textContent");
+  textarea.value = textContent;
+}
